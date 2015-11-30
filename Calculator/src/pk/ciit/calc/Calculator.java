9ce74b1989466f97c8c7215cd4cba42b9087a71c
sajid <sajid.shah.de@gmail.com>
2015-11-30 14:07:58 +0500

    calc

--- Calculator/src/pk/ciit/calc/Calculator.java
+++ Calculator/src/pk/ciit/calc/Calculator.java
@@ -1,11 +1,26 @@
 package pk.ciit.calc;
 
+/**
+ * This is the best calculator in the world
+ * @author sajid
+ * @since 30/11/2015
+ */
 public class Calculator {
-	Calculator(){
-		
+	/**
+	 * Default constructor
+	 */
+	public Calculator() {
+		// TODO Auto-generated constructor stub
 	}
-	int add(int a, int b){
-		return a+b;
+	
+	/**
+	 * This method calculates the sum of two numbers
+	 * @param num1 is the first number to be added
+	 * @param num2 is the second number to be added
+	 * @return the sum of both numbers
+	 */
+	public int sum(int num1, int num2){
+		return num1 + num2;
 	}
 
 }
