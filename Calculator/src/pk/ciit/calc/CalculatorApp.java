abc73aba5ff59fbb6a53c208a599b49e4eb044c9
sajid <sajid.shah.de@gmail.com>
2015-12-07 08:01:44 +0500

    calc

---
+++ Calculator/src/pk/ciit/calc/CalculatorApp.java
@@ -0,0 +1,11 @@
+package pk.ciit.calc;
+
+public class CalculatorApp {
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		System.out.println(calc.sum(5, 10));
+
+	}
+
+}
